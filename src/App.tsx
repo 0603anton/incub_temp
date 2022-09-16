69a86d74f56b8a6be295471335da985d926149d4
0603anton <zorgzb@gmail.com>
2022-09-16 14:12:13 +0200

    повотрение типизации todo 1st video done

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,53 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {TasksType, Todolist} from "./Todolist/Todolist";
+
+let tasks1:Array<TasksType> = [
+    {
+        isDone: true,
+        title: `CSS`,
+        id: 1
+    },
+    {
+        isDone: true,
+        title: `JS`,
+        id: 2
+    },
+    {
+        isDone: false,
+        title: `React`,
+        id: 3
+    }
+]
+let tasks2:Array<TasksType> = [
+    {
+        isDone: true,
+        title: `Грокаем`,
+        id: 4
+    },
+    {
+        isDone: true,
+        title: `Learn JS`,
+        id: 5
+    },
+    {
+        isDone: false,
+        title: `React documentation`,
+        id: 6
+    }
+]
 
 function App() {
+
     return (
         <div className="App">
             <Rating value={3}/>
             <Accordion value={`text for hading`} collapsed={true}/>
             <Accordion value={`false text for heading`} collapsed={false}/>
             <Rating value={4}/>
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="What to read" tasks={tasks2}/>
         </div>
     );
 }
